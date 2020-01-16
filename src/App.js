fd998861e8d667d289b299a92c48fa4871b0c62d
Tero Mäntylä <termanty@gmail.com>
2020-01-16 18:58:39 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App({notes}) {  
+  const rows = notes.map(note => <li>{note.content}</li>)
+
+  return (
+    <div className="App">
+      <h1>Notes</h1>
+      <ul>
+        {rows}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
